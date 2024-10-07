5f7397faccb730d88c6178d1341f28f08edb0465
Mark Lee <code0x9@gmail.com>
2024-10-07 21:57:58 +0900

    ..

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana, Single_Day} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['700'] });
+export const single_day = Single_Day({ weight: ['400'] });
